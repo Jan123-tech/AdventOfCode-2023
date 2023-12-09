1bc71f329f65e23e303f96c4bf28ea896f7c79ae
Jan123-tech <>
2023-12-09 15:29:19 +0000

    Cleanup

--- Day-08/Main-Part-02.hs
+++ Day-08/Main-Part-02.hs
@@ -13,11 +13,6 @@ main = do
 
     let items = lines contents
 
-    let s0 = [77241539 :: Integer, 154483078..]
-    let s1 = head $ filter (\x -> x `mod` 49803847 == 0) s0
-
-    print s1
-
     let instructions = head items
 
     let nodes0 = drop 2 items
@@ -25,15 +20,21 @@ main = do
     let nodes2 = map (\x -> let split = splitOn ", " $ snd x in (fst x, (drop 1 $ head split, let right = last split in take (length right - 1) right))) nodes1
 
     let nodeMap = Map.fromList nodes2
-
-    let startNodes = take 3 . drop 3 . map fst $ filter (\x -> drop 2 (fst x) == "A") nodes2
-
     let newKeys keys instr = map (\x -> let node = nodeMap ! x in if instr == 'L' then fst node else snd node) keys
 
+    let period1 = getPeriod 0 nodes2 newKeys instructions
+    let period2 = getPeriod 3 nodes2 newKeys instructions
+
+    print period1
+    print period2
+    
+    let s0 = [period2, 2*period2..]
+    let s1 = head $ filter (\x -> x `mod` period1 == 0) s0
+
+    print s1
+
+getPeriod :: Int -> [([Char], ([Char], [Char]))] -> ([[Char]] -> Char -> [[Char]]) -> String -> Int
+getPeriod d nodes2 newKeys instructions = do
+    let startNodes = take 3 . drop d . map fst $ filter (\x -> drop 2 (fst x) == "A") nodes2
     let moreNodes = scanl (\acc instr -> (fst acc + 1, newKeys (snd acc) instr)) (0 :: Int, startNodes) (cycle instructions)
-
-    let count = take 5 $ filter (all (\x -> drop 2 x == "Z") . snd) moreNodes
-
-    mapM_ print count
-
-    print 'v'
+    fst $ head $ take 1 $ filter (all (\x -> drop 2 x == "Z") . snd) moreNodes
